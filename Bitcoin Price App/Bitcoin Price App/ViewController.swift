4f9588eca5386708c910daa2d6c7da4c77dd8cc1
Rosh Sugathan Thaivalappil <roshsugathanthaivalappil@Roshs-MacBook-Pro.local>
2019-10-31 22:27:32 -0400

    Getting started

---
+++ Bitcoin Price App/Bitcoin Price App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitcoin Price App
+//
+//  Created by Rosh Sugathan Thaivalappil on 10/31/19.
+//  Copyright © 2019 Rosh Sugathan Thaivalappil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
