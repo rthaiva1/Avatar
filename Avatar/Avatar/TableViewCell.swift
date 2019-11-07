736009f48ba7c363559d1a7ded515e943e18d6b1
Rosh Sugathan Thaivalappil <roshsugathanthaivalappil@Roshs-MBP.lan1>
2019-11-06 22:51:00 -0500

    Table view cell customized for requirement

--- Avatar/Avatar/TableViewCell.swift
+++ Avatar/Avatar/TableViewCell.swift
@@ -2,29 +2,45 @@
 //  TableViewCell.swift
 //  Avatar
 //
-//  Created by Rosh Sugathan Thaivalappil on 11/4/19.
+//  Created by Rosh Sugathan Thaivalappil on 11/6/19.
 //  Copyright © 2019 Rosh Sugathan Thaivalappil. All rights reserved.
 //
 
 import UIKit
 
 class TableViewCell: UITableViewCell {
-
-    @IBOutlet weak var avatarLabel:UILabel?
-    @IBOutlet weak var avatarLevel:UILabel?
-    @IBOutlet weak var ownerLabel:UILabel?
-    @IBOutlet weak var priceLabel:UILabel?
+    
+    @IBOutlet var avatarimage: UIImageView!
+    @IBOutlet weak var avatarLabel: UILabel!
+    @IBOutlet weak var ownerLabel: UILabel!
+    @IBOutlet weak var avatarLevel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        Init()
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        // Initialization code
+        Init()
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    
+    func Init()
+    {
+        avatarLabel!.text = ""
+        avatarLevel!.text = ""
+        ownerLabel!.text = ""
+        priceLabel!.text = ""
+    }
+    
     func set(name: String,level: Int,owner: String?, price: Int)
     {
         avatarLabel!.text = name
